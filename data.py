1e83df01a442db529d61167478a1099a2e3c3495
jamesdsmith99 <36130181+jamesdsmith99@users.noreply.github.com>
2020-03-27 21:16:14 +0000

    added NoLabels dataset

--- data.py
+++ data.py
@@ -13,3 +13,13 @@ class SubCIFAR(CIFAR10):
         labels = np.array(labels)
         mask = np.in1d(labels, filter_classes)
         return data.compress(mask, axis=0), labels[mask].tolist()
+
+class NoLabels:
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx][0]
