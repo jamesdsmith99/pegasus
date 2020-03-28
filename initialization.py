19b3420c49812cec20d1931531d0087a75e1d31f
jamesdsmith99 <36130181+jamesdsmith99@users.noreply.github.com>
2020-03-28 16:50:48 +0000

    fixed initialization function to look for Conv2d to prevent trying to assign weight to my submodules

--- initialization.py
+++ initialization.py
@@ -2,7 +2,7 @@ from torch import nn
 
 def dcgan_init(m):
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find('Conv2d') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
