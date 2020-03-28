18e123c1a79cbc26fcaf9184bc721d2a70ddaee5
jamesdsmith99 <36130181+jamesdsmith99@users.noreply.github.com>
2020-03-28 11:58:27 +0000

    Discriminator uses HalfConv not HalfConvSpec

--- networks/discriminator.py
+++ networks/discriminator.py
@@ -9,11 +9,11 @@ class Discriminator(nn.Module):
 
         self.f = f
 
-        self.conv1 = HalfConvSpec(  3,   f, nn.LeakyReLU())  # 32x32 -> 16x16
-        self.conv2 = HalfConvSpec(  f, 2*f, nn.LeakyReLU())  # 16x16 -> 8x8
-        self.conv3 = HalfConvSpec(2*f, 4*f, nn.LeakyReLU())  # 8x8 -> 4x4
-        self.conv4 = HalfConvSpec(4*f, 8*f, nn.LeakyReLU())  # 4x4 -> 2x2
-        self.conv5 = HalfConvSpec(8*f,   1, nn.Sigmoid()) # 2x2 -> 1x1
+        self.conv1 = HalfConv(  3,   f, nn.LeakyReLU())  # 32x32 -> 16x16
+        self.conv2 = HalfConv(  f, 2*f, nn.LeakyReLU())  # 16x16 -> 8x8
+        self.conv3 = HalfConv(2*f, 4*f, nn.LeakyReLU())  # 8x8 -> 4x4
+        self.conv4 = HalfConv(4*f, 8*f, nn.LeakyReLU())  # 4x4 -> 2x2
+        self.conv5 = HalfConv(8*f,   1, nn.Sigmoid()) # 2x2 -> 1x1
 
     def feature(self, x):
         x = self.conv1(x)
